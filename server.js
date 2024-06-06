03c31153b8b1943597886fcf215fdf13d72422c2
Aleescobarok <alegescobar@hotmail.com>
2024-06-05 22:58:33 -0300

    Add protected route and JWT middleware

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
+
+// Configura dotenv para usar variables de entorno desde el archivo .env en la carpeta env
+dotenv.config({ path: path.join(__dirname, 'env', '.env') });
 
 const app = express();
 
-// Cargar variables de entorno directamente (solución temporal)
-const MONGO_URI = 'mongodb+srv://aescobararena:iu68chZpQEt3jbuB@aleescobarok.xdvarcm.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=aleescobarok';
-const JWT_SECRET = 'mysecretkey';
-
-// Verificar que la variable de entorno MONGO_URI esté definida
-if (!MONGO_URI) {
+// Verificar que las variables de entorno estén definidas
+if (!process.env.MONGO_URI) {
     throw new Error('Falta la variable de entorno MONGO_URI');
 }
 
+if (!process.env.JWT_SECRET) {
+    throw new Error('Falta la variable de entorno JWT_SECRET');
+}
+
 // Conectar a la base de datos
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
@@ -30,7 +35,7 @@ app.get('/', (req, res) => {
 
 // Definir rutas
 app.use('/api/auth', require('./routes/authRoutes'));
-// Define otras rutas según sea necesario
+app.use('/api', require('./routes/protectedRoute')); // Añadir la ruta protegida
 
 const PORT = process.env.PORT || 4000;
 
